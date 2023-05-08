076e60265c0b8c176e45c14aca8e8165fcb2c3b5
unknown <yasashibriliantp@gmail.com>
2023-05-08 09:59:47 +0700

    new

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import clientPromise from '@/lib/mongodb'
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
+  ],
+  adapter: MongoDBAdapter(clientPromise)
+})
